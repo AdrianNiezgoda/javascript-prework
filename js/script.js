9e237d15c3552f133ee1b9721610b9b648be1139
Adrian Niezgoda <adrian01234@gazeta.pl>
2023-05-15 18:18:57 +0200

    Change functions to anonymous functions

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-{function playGame(playerInput){
+{const playGame = function (playerInput){
     clearMessages();
-    function getMoveName(argMoveId){
+    const getMoveName = function (argMoveId){
         if(argMoveId == 1){
         return 'kamień';
         } else if (argMoveId == 2){
@@ -12,7 +12,7 @@
         printMessage('Nie znam ruchu o id ' + argMoveId + '.');
         return 'nieznany ruch';
     }
-    function displayResult(argComputerMove, argPlayerMove) {
+    const displayResult = function(argComputerMove, argPlayerMove) {
         console.log('moves:', argComputerMove, argPlayerMove);
         if (argComputerMove == "kamień" && argPlayerMove == "papier") {
         printMessage("Ty wygrywasz!");
